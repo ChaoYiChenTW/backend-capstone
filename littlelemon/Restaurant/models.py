a7cf4d49c8d4fdd04bfcd15a91eab927fa23960b
ChaoYiChen <a951159aca@gmail.com>
2025-07-09 09:05:20 +0000

    Build Restaurant Menu API.

--- littlelemon/Restaurant/models.py
+++ littlelemon/Restaurant/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Booking(models.Model):
     name = models.CharField(max_length=255)
@@ -7,8 +8,10 @@ class Booking(models.Model):
     booking_date = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.name} booked a table of {self.no_of_guests} at {self.booking_date}."
-    
+        return (
+            f"{self.name} booked a table of {self.no_of_guests} at {self.booking_date}."
+        )
+
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
--- littlelemon/Restaurant/views.py
+++ littlelemon/Restaurant/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Menu
+from .serializers import ModelSerializer
+
 
 # Create your views here.
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = ModelSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = ModelSerializer
+    permission_classes = [IsAuthenticated]
